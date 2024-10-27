e3d12f8d2a581e72f11336dde99a49f8ff910ffc
TechClusterHQ <136372560+TechClusterHQ@users.noreply.github.com>
2024-10-27 16:50:33 +0100

    feat: add slowban_banned_peers variable

--- root/clean-banlist.sh
+++ root/clean-banlist.sh
@@ -6,10 +6,16 @@ log() {
     echo "${timestamp} (qbt-slowban) (cleaner) ${message}"
 }
 
-response=$(curl --silent --request POST --url "http://localhost:${WEBUI_PORT}/api/v2/app/setPreferences" --data "json={\"banned_IPs\": \"\"}")
+if [[ -n "${SLOWBAN_BANNED_PEERS}" ]]; then
+    banned_ips=$(echo "${SLOWBAN_BANNED_PEERS}" | tr ',' '\n')
+else
+    banned_ips=""
+fi
+
+response=$(curl --silent --request POST --url "http://localhost:${WEBUI_PORT}/api/v2/app/setPreferences" --data-urlencode "json={\"banned_IPs\": \"${banned_ips}\"}")
 
 if [[ $? -eq 0 ]]; then
-    log "banlist cleared successfully"
+    log "banlist updated successfully"
 else
-    log "error clearing banlist"
+    log "error updating banlist"
 fi
